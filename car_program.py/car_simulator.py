eba2785ae1a3b1c6e0343dd805ca295d150baac3
Kimberly Alvarado <kimmyjugado@gmail.com>
2026-06-06 21:56:03 +0800

    finishing my program

--- car_program.py/car_simulator.py
+++ car_program.py/car_simulator.py
@@ -17,4 +17,19 @@ def run_telemetry_sim(car: Car, action: str, iterations: int = 5) -> None:
     print(f"\n Initializing Telemetry: {action.upper()} Phase")
     print("=" *45)
 
-    
+    for i in rnage(1, iterations + 1):
+        time.sleep(0.2) #for delay hehe
+        getattr(car, action.lower())()
+        bar = "❚" * (car.speed // 2)
+        print(f"Cycle {i} | Speed: {car.speed.02d} mph | {bar}")
+
+def main() -> None:
+    beast - Car(year_model="2026", make="CyberTruck")
+    print(f"System Boot.. Tracking 2026 {beast.Car__make} status.")
+    run_telemetry_sim(beast, action="accelerate")
+    run_telemetry_sim(beast, action="brake")
+
+    print("\n Simulation completed!")
+
+if __name__ == "__main__":
+    main()
